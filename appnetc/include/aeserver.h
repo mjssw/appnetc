4d8daa314cd7b4e2d388df890fe86ac74d140ab3
changbinliu <changbinliu@changbinliudeMacBook-Pro.local>
2016-01-21 22:43:17 +0800

    格式整理

--- appnetc/include/aeserver.h
+++ appnetc/include/aeserver.h
@@ -2,7 +2,7 @@
 #ifndef __AE_SERVER_H__
 #define __AE_SERVER_H__
 #include <time.h>
-#include <signal.h> 
+#include <signal.h>
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <stdlib.h>
@@ -16,13 +16,13 @@
 struct aeEventLoop;
 typedef struct _aeConnection
 {
-  int flags;
-  int fd;
-  char recv_buffer[10240];
-  int  recv_length; //last recv length
-  int  read_index;
-  char* client_ip;
-  int client_port;
+    int flags;
+    int fd;
+    char recv_buffer[10240];
+    int  recv_length; //last recv length
+    int  read_index;
+    char* client_ip;
+    int client_port;
 }aeConnection;
 
 typedef struct _aeServer aeServer;
@@ -30,25 +30,25 @@ typedef struct _aeReactor aeReactor;
 //reactor结构体
 struct _aeReactor
 {
-	void *object;
+    void *object;
     void *ptr;  //reserve
-	aeEventLoop *eventLoop;
-	
-	int epfd;
-	int id;
-	int event_num;
+    aeEventLoop *eventLoop;
+    
+    int epfd;
+    int id;
+    int event_num;
     int max_event_num;
     int running :1;
-	
-	aeConnection *socket_list;
-	//对事件操作
-	int (*add)(aeReactor *, int fd, int fdtype);
+    
+    aeConnection *socket_list;
+    //对事件操作
+    int (*add)(aeReactor *, int fd, int fdtype);
     int (*set)(aeReactor *, int fd, int fdtype);
     int (*del)(aeReactor *, int fd);
     int (*wait)(aeReactor *, struct timeval *);
     void (*free)(aeReactor *);
-
-	int (*write)(aeReactor *, int __fd, void *__buf, int __n);
+    
+    int (*write)(aeReactor *, int __fd, void *__buf, int __n);
     int (*close)(aeReactor *, int __fd);
 };
 
@@ -61,11 +61,11 @@ typedef struct _aeWorkerInfo
 
 typedef struct _workerBase
 {
-	int   pidx; //主进程中分的编号0-x
-	pid_t pid;
-	int pipefd;
-	int running;
-
+    int   pidx; //主进程中分的编号0-x
+    pid_t pid;
+    int pipefd;
+    int running;
+    
 }aWorkerBase;
 
 
@@ -79,31 +79,31 @@ typedef struct _aeReactorThread
 
 struct _aeServer
 {
-   char* listen_ip;
-   int listenfd;
-   int   port;
-   void *ptr2;
-   int reactorNum;
-   aeReactor* mainReactor;
-   aeConnection* connlist;
-   aeReactorThread *reactorThreads;
-   pthread_barrier_t barrier;
-   
-   int sigPipefd[2];
-
-   int  (*send)(  int fd, char* data , int len );
-   void (*close)( aeConnection *c  );   
-   void (*onConnect)( aeServer* serv ,int fd );
-   void (*onRecv)( aeServer *serv, aeConnection* c , int len );
-   void (*onClose)( aeServer *serv , aeConnection *c );
-   void (*runForever )( aeServer* serv );
+    char* listen_ip;
+    int listenfd;
+    int   port;
+    void *ptr2;
+    int reactorNum;
+    aeReactor* mainReactor;
+    aeConnection* connlist;
+    aeReactorThread *reactorThreads;
+    //pthread_barrier_t barrier;
+    
+    int sigPipefd[2];
+    
+    int  (*send)(  int fd, char* data , int len );
+    void (*close)( aeConnection *c  );
+    void (*onConnect)( aeServer* serv ,int fd );
+    void (*onRecv)( aeServer *serv, aeConnection* c , int len );
+    void (*onClose)( aeServer *serv , aeConnection *c );
+    void (*runForever )( aeServer* serv );
 };
 
 
 typedef struct _reactorThreadParam
 {
-	int thid;
-	aeServer* serv;
+    int thid;
+    aeServer* serv;
 }reactorThreadParam;
 
 
--- appnetc/socketserver.c
+++ appnetc/socketserver.c
@@ -9,43 +9,43 @@ aeServer* appnetTcpServInit( char* listen_ip , int port  );
 
 void appnetServerOnRecv( aeServer* s , aeConnection *c , int len )
 {
-	printf( "recv len=%d,data=%s,threadid=%d\n" ,strlen( c->recv_buffer ) , c->recv_buffer,pthread_self() );
-	//s->send( c->fd , c->recv_buffer, strlen( c->recv_buffer ) );
+    printf( "recv len=%d,data=%s,threadid=%d\n" , (unsigned )strlen( c->recv_buffer ) , c->recv_buffer,(unsigned)pthread_self() );
+    //s->send( c->fd , c->recv_buffer, strlen( c->recv_buffer ) );
 }
 
 void appnetServerOnClose( aeServer* s , aeConnection *c )
 {
-      printf( "close fd=%d,threadid=%d\n" , c->fd,pthread_self() );
-      s->close( c );
+    printf( "close fd=%d,threadid=%u\n" , c->fd,(unsigned)pthread_self() );
+    s->close( c );
 }
 
 void appnetServerOnConnect( aeServer* s , int fd )
 {
-    printf( "New Client Connected fd=%d,threadid=%d \n", fd,pthread_self()   );
-    char* buff = "connect ok!"; 
-    s->send( fd , buff , strlen( buff ) );
+    printf( "New Client Connected fd=%d,threadid=%u \n", fd,(unsigned)pthread_self()   );
+    char* buff = "connect ok!";
+    s->send( fd , buff , (unsigned)strlen( buff ) );
 }
 
 
 aeServer* appnetTcpServInit( char* listen_ip , int port  )
 {
-     serv = aeServerCreate( listen_ip , port );
-
-	 //回调改为从reactor线程中回调，如果加上worker后，在worker中回调。
-     serv->onConnect = 	&appnetServerOnConnect;
-     serv->onRecv = 	&appnetServerOnRecv;
-     serv->onClose = 	&appnetServerOnClose;
-     return serv;
+    serv = aeServerCreate( listen_ip , port );
+    
+    //回调改为从reactor线程中回调，如果加上worker后，在worker中回调。
+    serv->onConnect = 	&appnetServerOnConnect;
+    serv->onRecv = 	&appnetServerOnRecv;
+    serv->onClose = 	&appnetServerOnClose;
+    return serv;
 }
 
 void appnetTcpServRun()
 {
-     serv->runForever( serv );
+    serv->runForever( serv );
 }
 
 int main()
 {
-  aeServer* serv = appnetTcpServInit( "0.0.0.0" , 3011 );
-  appnetTcpServRun();
-return 0;
+    appnetTcpServInit( "0.0.0.0" , 3011 );
+    appnetTcpServRun();
+    return 0;
 }
